fa6d1f2907a2b44c1dd34caf1645e2e158d7807b
AlFahimBinFaruk <alfahim.ps@gmail.com>
2024-06-16 22:55:22 +0600

    db connection and evn setup done

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+export SECRET_KEY=dev
+export MONGO_URI="mongodb+srv://alfahimd3v:HDZq4BjuruJ6XNIV@cluster1.fa2ety3.mongodb.net/flaskbook?retryWrites=true&w=majority"
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,8 +1,42 @@
-from flask import Flask
+import os
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from bson import json_util
+import json
 
-app = Flask(__name__)
+mongo = PyMongo()
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get("SECRET_KEY"),
+        MONGO_URI=os.environ.get("MONGO_URI"),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # initializing Database configuration
+    mongo.init_app(app)
+
+    # a simple page that says hello
+    @app.get('/')
+    def hello():
+        from .db_models.User import User
+        nw = User("fahim", "doe2", "do2e@gmail.com", "33333")
+        nw.save()
+        return 'Hello, World!'
+
+    return app
